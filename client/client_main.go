61dd8fe7982172ecbf5c950123745b9d7e9c942c
Haocheng Wang <wang.haocheng@yahoo.com>
2016-08-08 16:57:34 +0800

    Initial commitment.

---
+++ client/client_main.go
@@ -0,0 +1,10 @@
+// server_main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ server/server_main.go
@@ -0,0 +1,10 @@
+// client_main.go
+package client_main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
